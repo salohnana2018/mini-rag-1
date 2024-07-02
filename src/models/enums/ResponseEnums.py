e1b17c8dc4abd5673e98b23a810b1d9dd86c95d8
Abu Bakr <64726450+abubakr-soliman@users.noreply.github.com>
2024-07-02 18:12:19 +0300

    update file processing endpoint

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -9,3 +9,5 @@ class ResponseSignal(Enum):
     FILE_UPLOAD_FAILED = "file_upload_failed"
     PROCESSING_SUCCESS = "processing_success"
     PROCESSING_FAILED = "processing_failed"
+    NO_FILES_ERROR = "not_found_files"
+    FILE_ID_ERROR = "no_file_found_with_this_id"
