ece7a7c17fd415b0c65ecc51d793081fa94dfcdf
Abu Bakr <64726450+abubakr-soliman@users.noreply.github.com>
2024-08-15 10:52:33 +0300

    update LLM providers

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -24,9 +24,10 @@ class OpenAIProvider(LLMInterface):
 
         self.client = OpenAI(
             api_key = self.api_key,
-            api_url = self.api_url
+            base_url = self.api_url if self.api_url and len(self.api_url) else None
         )
 
+        self.enums = OpenAIEnums
         self.logger = logging.getLogger(__name__)
 
     def set_generation_model(self, model_id: str):
@@ -68,7 +69,7 @@ class OpenAIProvider(LLMInterface):
             self.logger.error("Error while generating text with OpenAI")
             return None
 
-        return response.choices[0].message["content"]
+        return response.choices[0].message.content
 
 
     def embed_text(self, text: str, document_type: str = None):
