5d175b4a5385f2b440bd72cf06be05bbf7402135
Abu Bakr <64726450+abubakr-soliman@users.noreply.github.com>
2024-09-23 07:28:37 +0300

    fix rag template issues

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -113,7 +113,9 @@ class NLPController(BaseController):
             for idx, doc in enumerate(retrieved_documents)
         ])
 
-        footer_prompt = self.template_parser.get("rag", "footer_prompt")
+        footer_prompt = self.template_parser.get("rag", "footer_prompt", {
+            "query": query
+        })
 
         # step3: Construct Generation Client Prompts
         chat_history = [
